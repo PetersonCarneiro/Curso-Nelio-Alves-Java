073a96d0c64a64c5d6d153d57680dee2e64c36d1
Peter <peters.prestes@gmail.com>
2022-06-09 13:01:10 -0300

    atualização 09/06/2022

--- Aula 223 Generics/src/application/Program.java
+++ Aula 223 Generics/src/application/Program.java
@@ -1,5 +1,32 @@
 package application;
 
+import java.util.Scanner;
+
+import services.PrintService;
+
 public class Program {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		PrintService<Integer> ps = new PrintService<>();
+		
+		System.out.println("How many values: ");
+		int n = sc.nextInt();
+		
+		for(int i=1;i<n;i++) {
+			System.out.println("Enter the value: ");
+			int value = sc.nextInt();
+			ps.addValue(value);
+			
+			ps.print();
+			Integer x = ps.first();
+			System.out.println("Firt: "+x);
+			
+			sc.close();
+		}
+	}
 
 }
